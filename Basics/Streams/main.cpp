ec7e43e14ab8eb8e67616b5af48105c7279b3e62
ilyayunkin <ilya.yunkin@ya.ru>
2020-11-14 18:57:21 +0600

    iostream example

---
+++ Basics/Streams/main.cpp
@@ -0,0 +1,33 @@
+#include <QCoreApplication>
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    {
+        cout << "Input a float. f= ";
+        float f;
+        cin >> f;
+        cout << "You've input " << f << '\n';
+    }
+    {
+        cout << "Input an int. i= ";
+        int i;
+        cin >> i;
+        cout << "You've input " << i << '\n';
+    }
+    {
+        cout << "Input three ints ";
+        int i[3];
+        cin >> i[0] >> i[1] >> i[2];
+        cout << "You've input";
+        for(auto const ii : i)
+        {
+            cout << ' ' << ii;
+        }
+        cout << '\n';
+    }
+
+    return a.exec();
+}
