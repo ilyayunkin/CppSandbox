dd7b2bed613b820f9830e7cc01e5526fb909919b
ilyayunkin <ilya.yunkin@ya.ru>
2020-11-14 19:32:18 +0600

    Float to Int casting example added by floor, ceil and round

--- Casts/FloatToIntCast/main.cpp
+++ Casts/FloatToIntCast/main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 int main(int argc, char *argv[])
@@ -10,6 +11,21 @@ int main(int argc, char *argv[])
     cout << int(5.51) << '\n';
     cout << int(5.0) << '\n';
     cout << int(5.99) << '\n';
+    cout << "round\n";
+    cout << round(5.49) << '\n';
+    cout << round(5.51) << '\n';
+    cout << round(5.0) << '\n';
+    cout << round(5.99) << '\n';
+    cout << "ceil\n";
+    cout << ceil(5.49) << '\n';
+    cout << ceil(5.51) << '\n';
+    cout << ceil(5.0) << '\n';
+    cout << ceil(5.99) << '\n';
+    cout << "floor\n";
+    cout << floor(5.49) << '\n';
+    cout << floor(5.51) << '\n';
+    cout << floor(5.0) << '\n';
+    cout << floor(5.99) << '\n';
 
     return a.exec();
 }
